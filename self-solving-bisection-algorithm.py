d53232be8806837811862f870278b776cff3c344
Eduard Belan <eduard.belan.git@gmail.com>
2023-06-13 10:12:42 +0200

    improve go_again syntax and add logo to numgg machine version

--- self-solving-bisection-algorithm.py
+++ self-solving-bisection-algorithm.py
@@ -1,20 +1,28 @@
 import random
-
+import sys
+import os
+from machine_art import logo
 
 EASY_MODE = 10
 HARD_MODE = 5
 
 
 def guessing(lives):
-    """Lives Counter: - 1"""
+    """
+    Lives Counter: - 1"
+    """
     return lives - 1
 
 
 def difficulty():
-    """User picks a difficulty"""
+    """
+    User picks a difficulty
+    """
     valid_input = False
+
     user_choice_difficulty = input(
-        "Type 'easy' for 10 lives, type 'hard' for 5 lives\nChoose a difficulty for 'The Machine': ")
+        "\nType 'easy' for 10 lives, type 'hard' for 5 lives\nChoose a difficulty for 'The Machine': ")
+
     while not valid_input:
         if user_choice_difficulty == "easy":
             return EASY_MODE
@@ -25,6 +33,9 @@ def difficulty():
 
 
 def bisection_algorithm(target, epsilon, lives):
+    """
+    Bisection Algorithm for the Machine to solve the Game
+    """
     num_guesses = 0
     low = 1
     high = 100
@@ -40,6 +51,7 @@ def bisection_algorithm(target, epsilon, lives):
         guess = (high + low) // 2
         num_guesses += 1
         lives = guessing(lives)
+
         if lives == 0:
             print("'The Machine' loses.")
             break
@@ -50,8 +62,16 @@ def bisection_algorithm(target, epsilon, lives):
 
 
 def number_guessing_game():
-    """Just the game, nothing else."""
+    """
+    Just the game, nothing else
+    """
+    clear = lambda: os.system("cls")
+    clear()
+
     random_number = random.choice(range(1, 101))
+
+    print(logo)
+    print(f"                                      This is the secret number: {random_number}\n")
 
     user_lives = difficulty()
     print(f"\n'The Machine' has {user_lives} lives.")
@@ -63,6 +83,26 @@ def number_guessing_game():
     else:
         print(f"\nThe correct number is {random_number}.")
 
+    go_again()
 
-while input("Do you want 'The Machine' to play the Number-Guessing-Game? Type 'y' or 'n': ") == "y":
-    number_guessing_game()
+
+def go_again():
+    """
+    Ask the User to play again
+    """
+    valid_input = False
+
+    while not valid_input:
+        another_one = input("\nDo you want 'The Machine' to play the Number-Guessing-Game? (y/n): ")
+
+        if another_one not in ["y", "n"]:
+            print("\nInvalid Input.\n ")
+        elif another_one == "n":
+            clear = lambda: os.system("cls")
+            clear()
+            sys.exit()
+        else:
+            number_guessing_game()
+
+
+go_again()
